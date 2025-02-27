1ee02cf5072f317f2d2b914204bd8d58e5292436
khang <vovankhang184@gmail.com>
2025-02-27 22:13:01 +0700

    Design table Book

---
+++ src/app/(public)/admin/book/filter-book.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Form, Input, Button } from "antd";
+
+const FilterBook: React.FC<{
+  onSearch: (values: any) => void;
+  onReset: () => void;
+}> = ({ onSearch, onReset }) => {
+  const [form] = Form.useForm();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <Form
+      form={form}
+      layout="horizontal"
+      onFinish={(values) => {
+        // console.log("Form Submitted", values);
+        onSearch(values);
+      }}
+      initialValues={{ bookName: "", author: "" }}
+    >
+      <div className="flex justify-between gap-x-4">
+        <div className="basis-2/3 flex gap-x-[100px]">
+          <Form.Item
+            name="bookName"
+            label="Tên Sách"
+            className="flex-1 text-body1 "
+          >
+            <Input placeholder="Vui lòng nhập" className="w-[300px]" />
+          </Form.Item>
+        </div>
+
+        <div className="basis-1/3 flex justify-end gap-x-[15px]">
+          <Button
+            onClick={() => {
+              form.resetFields();
+              onReset();
+            }}
+            className="border border-gray-300"
+          >
+            Tải Lại
+          </Button>
+          <Button type="primary" onClick={() => form.submit()} className="bg-blue-500">
+            Tìm Kiếm
+          </Button>
+        </div>
+      </div>
+    </Form>
+  );
+};
+
+export default FilterBook;
